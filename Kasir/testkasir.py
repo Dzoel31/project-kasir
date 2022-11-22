2f7b7e7592773556501ecadd30e31b213105ceec
Dzulfikri Adjmal <82845859+Dzoel31@users.noreply.github.com>
2022-11-22 08:46:50 +0700

    Update testkasir.py

--- Kasir/testkasir.py
+++ Kasir/testkasir.py
@@ -1,72 +1,73 @@
 import os
 import pandas as pd
-import prettytable as pt
 
-def table_barang():
-    open_file = open("data_barang.csv")
-    table_barang = pt.from_csv(open_file)
-    print(table_barang)
+def save_data(data_barang):
+    saved_data = pd.DataFrame(data_barang, columns=[
+                            "nama_barang", "jenis_barang", "harga"])
+    saved_data.to_csv("data_barang.csv", index=False)
 
-def menu_edit():
-    def save_data(data_barang):
-        saved_data = pd.DataFrame(data_barang, columns=[
-                             "nama_barang", "jenis_barang", "harga"])
-        saved_data.to_csv("data_barang.csv", index=False)
+def curr_data(new_data):
+    current_data = pd.read_csv("data_barang.csv")
+    data_barang = current_data.values.tolist()
 
-    def curr_data(new_data):
-        current_data = pd.read_csv("data_barang.csv")
-        data_barang = current_data.values.tolist()
+    data_barang.append(new_data)
+    save_data(data_barang)
 
-        data_barang.append(new_data)
-        save_data(data_barang)
+def add_data():
+    nama_barang = input("Masukkan nama barang : ")
+    jns_barang = input("Masukkan jenis barang : ")
+    harga = int(input("Masukkan harga barang : "))
+    new_data = [nama_barang, jns_barang, harga]
+    curr_data(new_data)
 
-    def add_data():
-        nama_barang = input("Masukkan nama barang : ")
-        jns_barang = input("Masukkan jenis barang : ")
-        harga = int(input("Masukkan harga barang : "))
-        new_data = [nama_barang, jns_barang, harga]
-        curr_data(new_data)
-
-    def show_data():
-        current_data = pd.read_csv("data_barang.csv")
-        data_barang = current_data.values.tolist()
-        if data_barang == []:
-            print("Tidak ada data")
-            return "Tidak ada data"
-        else:
-            table_barang()
-        
-    def update_harga():
-        if show_data() == "Tidak ada data":
-            print("Tidak ada data")
-            print("Buat data terlebih dahulu!")
-        else:
-            try:
-                current_data = pd.read_csv("data_barang.csv")
-                data_barang = current_data.values.tolist()
-                pilih = int(input("Pilih data yang mau di perbaharui :"))
-                upd_harga = int(input("Masukkan harga baru : "))
-
-                data_barang[pilih][2] = upd_harga
-                save_data(data_barang)
-
-            except ValueError:
-                print("Input harus Integer!!!")
-                update_harga()
-                
-    def delete_data():
-        show_data()
+def show_data():
+    current_data = pd.read_csv("data_barang.csv")
+    data_barang = current_data.values.tolist()
+    if data_barang == []:
+        print("Tidak ada data")
+        return "Tidak ada data"
+    else:
+        nama_barang,jenis_barang,harga = map(list,zip(*data_barang))
+        print("-"*54)
+        print("|{:^4}|{:^15}|{:^15}|{:^15}|".format(
+            "No", "Nama Barang", "Jenis Barang", "Harga"))
+        print("-"*54)
+        for i in range(len(data_barang)):
+            print("|{:^4}|{:^15}|{:^15}|{:^15}|".format(i,nama_barang[i],jenis_barang[i],harga[i]))
+        print("-"*54)
+    
+def update_harga():
+    if show_data() == "Tidak ada data":
+        print("Tidak ada data")
+        print("Buat data terlebih dahulu!")
+    else:
         try:
             current_data = pd.read_csv("data_barang.csv")
             data_barang = current_data.values.tolist()
-            pilih = int(input("Pilih data yang mau dihapus : "))
-            data_barang.pop(pilih)
-            print("Data berhasil dihapus!!")
+            pilih = int(input("Pilih data yang mau di perbaharui : "))
+            upd_harga = int(input("Masukkan harga baru : "))
+
+            data_barang[pilih][2] = upd_harga
             save_data(data_barang)
+
         except ValueError:
-            print("Salah inputan")
-            delete_data()
-    
+            print("Input harus Integer!!!")
+            update_harga()
+            
+def delete_data():
+    show_data()
+    try:
+        current_data = pd.read_csv("data_barang.csv")
+        data_barang = current_data.values.tolist()
+        pilih = int(input("Pilih data yang mau dihapus : "))
+        data_barang.pop(pilih)
+        print("Data berhasil dihapus!!")
+        save_data(data_barang)
+    except ValueError:
+        print("Salah inputan")
+        delete_data()
+
+def menu_edit():
     os.system('cls')
     print("-"*20)
     print("[1] Buat Data/Tambah Data")
@@ -100,42 +101,43 @@ def menu_edit():
 
 def pembelian():
     os.system('cls')
-    current_data = pd.read_csv("data_barang.csv",usecols=["nama_barang","harga"])
-    data_barang = dict(current_data.values.tolist())
-    nm_barang = current_data["nama_barang"]
-    db_nm_barang = nm_barang.values.tolist()
-
+    current_data = pd.read_csv("data_barang.csv")
+    data_barang = current_data.values.tolist()
+    nama_barang,jenis_barang,harga = map(list,zip(*data_barang))
     barang_beli = []
+    jumlah_beli = []
     total = 0
-    ulang = True
-    while ulang == True:
+    ulang = 1
+    while ulang == 1:
         os.system("cls")
-        table_barang()
-        print(data_barang)
+        print("-"*54)
+        print("|{:^4}|{:^15}|{:^15}|{:^15}|".format(
+                    "No", "Nama Barang", "Jenis Barang", "Harga"))
+        print("-"*54)
+        for i in range(len(data_barang)):
+            print("|{:^4}|{:^15}|{:^15}|{:^15}|".format(i,nama_barang[i],jenis_barang[i],harga[i]))
+        print("-"*54)
+        barang,jumlah = input("Masukkan barang yg dibeli (Namabarang,jumlah beli): ").split(",")
 
-        barang_cust,jumlah_beli = input("Masukkan barang yg dibeli (Namabarang,jumlah beli): ").split(",")
-
-        new_barang = [barang_cust,int(jumlah_beli)]
-        barang_beli.append(new_barang)
-
+        barang_beli.append(barang)
+        jumlah_beli.append(int(jumlah))
 
         tambah =  input("Tambah barang? [y/n] : ")
         if (tambah == "N") or (tambah =="n"):
             break
+        else:
+            continue
     
+
     print("\n{:^61}".format("Struk Belanja"))
     print("-"*61)
     print("|{:^4}|{:^15}|{:^12}|{:^12}|{:^12}|".format("No","Nama Barang","Harga","Jumlah","Total"))
     print("-"*61)
-
     for k in range(len(barang_beli)):
-        if barang_beli[k][0] in db_nm_barang:
-            index = db_nm_barang.index(barang_beli[k][0])
-            print("|{:^4}|{:^15}|{:^12}|{:^12}|{:^12}|".format(k,barang_beli[k][0],
-            data_barang[index][2],
-            barang_beli[k][1],
-            (barang_beli[k][1]*data_barang[index][2])))
-            total += (barang_beli[k][1]*data_barang[index][2])
+        if barang_beli[k] in nama_barang:
+            index = nama_barang.index(barang_beli[k])
+            print("|{:^4}|{:^15}|{:^12}|{:^12}|{:^12}|".format(k,barang_beli[k],harga[index],jumlah_beli[k],(jumlah_beli[k]*harga[index])))
+            total += (jumlah_beli[k]*harga[index])
         else:
             print("Maaf, barang tersebut tidak ada di database kami")
             print("Silahkan masukkan ulang ")
